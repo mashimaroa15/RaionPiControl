bbb622318173702c99d8143a71ef086e5f228cca
mashimaroa15 <rabbit.bulk@gmail.com>
2017-04-17 23:28:09 +0200

    update update script

--- scripts/update_RaionPiControl.sh
+++ scripts/update_RaionPiControl.sh
@@ -1,9 +1,29 @@
+#!/usr/bin/env bash
+# version 0.1
+DIR="/home/pi/RaionPiControl/"
+WWW="/var/www/html"
+RAIONPICONTROL="https://github.com/mashimaroa15/RaionPiControl.git"
+
+if [ -d "$DIR" ]; then
+  echo ${DIR} exists &&
+  cd ${DIR} &&
+  git pull &&
+  cd ~
+else
+  echo ${DIR} doesn\'t exist &&
+  echo Clone new Git... &&
+  git clone ${RAIONPICONTROL} ${DIR}
+fi &&
+
 echo Remove all files... &&
-cd /var/www/html/ && rm -rf * && ls -A1 | xargs rm -rf &&
-echo Clone new Git... &&
-git clone https://github.com/mashimaroa15/RaionPiControl.git /var/www/html/ &&
+cd ${WWW} && rm -rf * && ls -A1 | xargs rm -rf &&
+
+echo Sync files with WWW directory... &&
+rsync -a --partial --delete ${DIR} ${WWW} &&
+
 echo Change ownership... &&
-chown -R pi:web /var/www/html/ &&
+chown -R pi:web ${WWW} &&
+
 echo Change permissions... &&
-chmod -R 775 /var/www/html/ &&
+chmod -R 775 ${WWW} &&
 cd ~
